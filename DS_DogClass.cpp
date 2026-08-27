fc0e405f81c119a5050dac88eddd5b9379622a9e
amilford <amilford@atu.edu>
2026-08-27 10:14:15 -0500

    Created the Dog class, added get/set functions, added bark function, and added a demo in the main function.

--- DS_DogClass.cpp
+++ DS_DogClass.cpp
@@ -3,7 +3,59 @@
 
 #include <iostream>
 using namespace std;
+
+class Dog
+{
+public:
+    string getName()
+    {
+        return name;
+    }
+
+    int getAge()
+    {
+        return age;
+    }
+
+    string getBreed()
+    {
+        return breed;
+    }
+
+    void setName(string dogName)
+    {
+        name = dogName;
+    }
+
+    void setAge(int dogAge)
+    {
+        age = dogAge;
+    }
+
+    void setBreed(string dogBreed)
+    {
+        breed = dogBreed;
+    }
+
+    void bark()
+    {
+        cout << "Woof! Woof!";
+    }
+
+private:
+    string name{ "Duncan" }, breed{ "Golden Retriever" };
+    int age{ 2 };
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Dog perfectDog;
+    int tempAge;
+    cout << "Name: " << perfectDog.getName() << endl;
+    cout << "Age: " << perfectDog.getAge() << endl;
+    cout << "Breed: " << perfectDog.getBreed() << endl << endl;
+    cout << "How old/young would you like to make " << perfectDog.getName() << "?" << endl;
+    cin >> tempAge;
+    perfectDog.setAge(tempAge);
+    cout << perfectDog.getName() << " is now " << perfectDog.getAge() << " year(s) old!";
 }
